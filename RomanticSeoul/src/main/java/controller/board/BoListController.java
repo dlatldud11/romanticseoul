f4657600db597528173680715ab1de485affc6ca
juilp <juilp@LAPTOP-5NICBO7S>
2021-06-24 15:28:59 +0900

    11

--- RomanticSeoul/src/main/java/controller/board/BoListController.java
+++ RomanticSeoul/src/main/java/controller/board/BoListController.java
@@ -1,5 +1,39 @@
 package controller.board;
 
-public class BoListController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import controller.common.SuperClass;
+//import dao.BoardDao;
+
+@Controller
+public class BoListController extends SuperClass{
+	private final String command = "/boList.bo" ; 
+	private ModelAndView mav = null ;
+	private String redirect = "./main.co" ;
+	
+//	@Autowired
+//	@Qualifier("bdao")
+//	private BoardDao dao ;
+	
+	public BoListController() {
+		super("boList", "boList");
+		this.mav = new ModelAndView();
+	}
+	
+	@GetMapping(command)
+	public ModelAndView doGet(){		
+		this.mav.setViewName(super.getpage);
+		return this.mav ;
+	}
+	
+	@PostMapping(command)
+	public ModelAndView doPost(){
+		this.mav.setViewName(super.postpage);
+		return this.mav ;
+	}
 }
