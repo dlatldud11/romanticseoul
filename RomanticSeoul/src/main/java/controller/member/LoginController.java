b4acd1c41af1cc64b3e38cf4043e43ed51d951d0
juilp <juilp@LAPTOP-5NICBO7S>
2021-06-25 13:43:09 +0900

    625 13:42

--- RomanticSeoul/src/main/java/controller/member/LoginController.java
+++ RomanticSeoul/src/main/java/controller/member/LoginController.java
@@ -1,5 +1,37 @@
 package controller.member;
 
-public class LoginController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import controller.common.SuperClass;
+//import dao.MemberDao;
+
+@Controller
+public class LoginController extends SuperClass {
+	private final String command = "/meLoginForm.me" ;
+	private ModelAndView mav = null ;
+	private final String redirect = "redirect/main.co" ;
+	
+//	@Autowired
+//	@Qualifier("mdao")
+//	private MemberDao mdao  ; 
+	
+	public LoginController() {
+		super("요기", "저기");
+		this.mav = new ModelAndView();
+	}
+	
+	@GetMapping(command)
+	public ModelAndView doGet() {
+		return this.mav ;
+	}
+	
+	@PostMapping(command)
+	public ModelAndView doPost() {
+		return this.mav ;
+	}	
 }
