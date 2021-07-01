0d8938f20837f3828422bca7323f95783f468d94
dlatldud11 <ssh456852@naver.com>
2021-07-01 17:51:26 +0900

    0701 17:51 ISY commit

--- RomanticSeoul/src/main/java/utility/DrinkAPI.java
+++ RomanticSeoul/src/main/java/utility/DrinkAPI.java
@@ -15,6 +15,10 @@ import bean.Store;
 
 public class DrinkAPI {
 //   public static void main(String[] args) {
+//	   List<String> list = getDrinkdistinct();
+//	   for ( String bean : list) {
+//		   System.out.println(bean);
+//	   }
 //      int page = 1;
 //      try {
 //         while(true)
@@ -54,6 +58,7 @@ public class DrinkAPI {
 //         e.printStackTrace();
 //      }
 //   }
+   
    private String getTagValue(String tag, Element eElement) {
       NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
       
@@ -103,6 +108,51 @@ public class DrinkAPI {
 	      }catch(Exception e) {
 	         e.printStackTrace();
 	      }
+	   return drinklists;
+   }
+   
+   
+   public List<String> getDrinkdistinct(){
+	   ArrayList<String> drinklists = new ArrayList<String>();
+	   int page = 1;
+	   try {
+		   while(true)
+		   {
+			   String url = "http://openapi.seoul.go.kr:8088/7571686f456e6e6d3430446f4d637a/xml/LOCALDATA_072405/1/1000/"+page;
+			   
+			   DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			   DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			   Document doc = dBuilder.parse(url);
+			   doc.getDocumentElement().normalize();
+			   
+			   System.out.println("Root element : " + doc.getDocumentElement().getNodeName());
+			   
+			   NodeList nList = doc.getElementsByTagName("row");
+			   
+			   System.out.println("파싱할 리스트 수 : "+nList.getLength());
+			   
+			   for(int temp = 0; temp < nList.getLength(); temp++) {
+				   Node nNode = nList.item(temp);
+				   
+				   if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+					   Element eElement = (Element) nNode;
+					   System.out.println("업태구분명 : " + getTagValue("UPTAENM" ,eElement));
+					   if(!drinklists.contains(getTagValue("UPTAENM" ,eElement))) {
+						   drinklists.add(getTagValue("UPTAENM" ,eElement));
+					   }else {
+						   System.out.println("중복된 업태구분명");
+					   }
+				   }
+			   }
+			   page += 1;
+			   System.out.println("page number " + page);
+			   if(page > 1) {
+				   break;
+			   }
+		   }
+	   }catch(Exception e) {
+		   e.printStackTrace();
+	   }
 	   return drinklists;
    }
 public ArrayList<Store> getdrinkGulist(String gu) {
